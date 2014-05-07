d36bf1990fd1a6a3dcbddb57fdf85282191345f4
jschwartz73 <jeff@schwartz-tribe.com>
2014-05-07 18:52:26 -0400

    Cleaned up plugin

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -32,76 +32,17 @@ public class Application extends Controller {
         return ok(result);
     }
 
-    public static Result index2() {
+    public static Result index() {
         ServiceInstance instance = CuratorDiscovery.getService(queryServiceName);
-        Logger.info("index2.instance: " + instance);
         if (instance == null) {
             return ok("NONE FOUND");
         } else {
-            return ok(instance.buildUriSpec());
+            return ok(callService(instance.buildUriSpec()));
         }
-    }
-
-    public static Result index() {
-
-//        String zooServers = Configuration.root()
-//                .getConfig("curator.discovery")
-//                .getString("zooServers", "localhost:2181");
-
-        // This shows how to query all the instances in service discovery
-        StringBuilder output = new StringBuilder("Searching for: " + queryServiceName + "\n");
-
-        ServiceDiscovery<InstanceDetails> serviceDiscovery = null;
-        CuratorFramework client = null;
-        try
-        {
-            String zooServers = "localhost:2181";
-
-            client = CuratorFrameworkFactory.newClient(zooServers, new ExponentialBackoffRetry(1000, 3));
-            client.start();
-
-            JsonInstanceSerializer<InstanceDetails> serializer = new JsonInstanceSerializer<InstanceDetails>(InstanceDetails.class);
-            serviceDiscovery = ServiceDiscoveryBuilder
-                    .builder(InstanceDetails.class)
-                    .client(client)
-                    .basePath(CuratorDiscovery.getPath())
-                    .serializer(serializer)
-                    .build();
-            serviceDiscovery.start();
-
-            Collection<ServiceInstance<InstanceDetails>> instances = serviceDiscovery.queryForInstances(queryServiceName);
-
-            for ( ServiceInstance<InstanceDetails> instance : instances )
-            {
-                Logger.info("Payload: " + instance.getPayload());
-                Logger.info("uri: " + instance.buildUriSpec());
-
-                String responseData = callService(instance.buildUriSpec());
-                Logger.info("response: " + responseData);
-
-                output.append("Found instance: " + instance.buildUriSpec() + ", payload: " + instance.getPayload());
-                output.append("\n************** OUTPUT ************");
-                output.append("\n" + responseData);
-                output.append("\n**********************************");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (serviceDiscovery != null) {
-                CloseableUtils.closeQuietly(serviceDiscovery);
-            }
-
-            if (client != null) {
-                CloseableUtils.closeQuietly(client);
-            }
-        }
-
-        return ok(output.toString());
     }
 
     public static String callService(String uri) {
-//        F.Promise<WS.Response> results = WS.url(uri + "/api/v1/echo").get();
-//        return results.get(1500).getBody();
-        return "*** NA ** JSS ***";
+        F.Promise<WS.Response> results = WS.url(uri + "/api/v1/echo").get();
+        return results.get(1500).getBody();
     }
 }
